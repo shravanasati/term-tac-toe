471d035b58eac4023116ffc29cf62e726b3cfe92
Shravan Asati <dev.shravan@protonmail.com>
2025-12-14 22:07:21 +0530

    add logs to redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -1,4 +1,10 @@
 set -euo
+
+echo "==> Pulling latest repository changes..."
 git pull
+
+echo "==> Bringing down existing services..."
 docker compose -f ./src/server/compose.yml down
+
+echo "==> Building and deploying new services..."
 docker compose -f ./src/server/compose.yml up --build -d
